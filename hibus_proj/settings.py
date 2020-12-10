601fdf1cfec502f58efc1375ebd9fb1ed24da7e4
Jiafei Peng <jiafeipeng92@gmail.com>
2020-12-10 09:55:04 +0800

    config CORS

--- hibus_proj/settings.py
+++ hibus_proj/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'hibus_proj.hibus',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,3 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# CORS configuration
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+# CORS_ALLOWED_ORIGIN_REGEXES = []
+
+# CORS_ALLOW_ALL_ORIGINS = False
