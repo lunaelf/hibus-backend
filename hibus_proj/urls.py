07cf54367ef511154f83e8864663eb74705caa26
Jiafei Peng <jiafeipeng92@gmail.com>
2020-12-10 19:28:40 +0800

    add api-auth

--- hibus_proj/urls.py
+++ hibus_proj/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('hibus_proj.hibus.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
