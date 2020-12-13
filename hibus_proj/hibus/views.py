68135c213886b678361d1cc934f85d7c7d9efa3b
Jiafei Peng <jiafeipeng92@gmail.com>
2020-12-13 16:58:13 +0800

    line and bus model add filter fields

--- hibus_proj/hibus/views.py
+++ hibus_proj/hibus/views.py
@@ -35,7 +35,9 @@ class LineList(generics.ListCreateAPIView):
     queryset = Line.objects.all()
     serializer_class = LineSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['station_start', 'station_end']
+    filterset_fields = ['url', 'id', 'station_start', 'station_end', 'estimate_hour', 'estimate_minute',
+                        'start_latitude', 'start_longitude', 'start_location', 'end_latitude',
+                        'end_longitude', 'end_location']
 
 
 class LineDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -46,6 +48,10 @@ class LineDetail(generics.RetrieveUpdateDestroyAPIView):
 class BusList(generics.ListCreateAPIView):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['url', 'id', 'line_id', 'bus_number', 'ticket_price', 'threshold_price',
+                        'current_passenger', 'threshold_passenger', 'latitude', 'longitude',
+                        'location', 'station', 'status', 'driver_phone']
 
 
 class BusDetail(generics.RetrieveUpdateDestroyAPIView):
