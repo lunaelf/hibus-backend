4934951df0c2333a740ddc78900eb7c91caef69b
Jiafei Peng <jiafeipeng92@gmail.com>
2020-12-15 09:01:08 +0800

    remove hyperlink

--- hibus_proj/hibus/serializers.py
+++ hibus_proj/hibus/serializers.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from hibus_proj.hibus.models import Line, Bus, Order, CustomUser
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['url', 'id', 'username']
+        fields = ['id', 'username']
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -17,28 +17,27 @@ class CustomUserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'password', 'is_admin']
 
 
-class LineSerializer(serializers.HyperlinkedModelSerializer):
-
+class LineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Line
-        fields = ['url', 'id', 'station_start', 'station_end', 'estimate_hour', 'estimate_minute',
+        fields = ['id', 'station_start', 'station_end', 'estimate_hour', 'estimate_minute',
                   'start_latitude', 'start_longitude', 'start_location', 'end_latitude',
                   'end_longitude', 'end_location']
 
 
-class BusSerializer(serializers.HyperlinkedModelSerializer):
+class BusSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Bus
-        fields = ['url', 'id', 'line_id', 'bus_number', 'ticket_price', 'threshold_price',
+        fields = ['id', 'line_id', 'bus_number', 'ticket_price', 'threshold_price',
                   'current_passenger', 'threshold_passenger', 'latitude', 'longitude',
                   'location', 'station', 'status', 'driver_phone']
 
 
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ['url', 'id', 'user_id', 'bus_id', 'line_id', 'order_number', 'base_order_number',
+        fields = ['id', 'user_id', 'bus_id', 'line_id', 'order_number', 'base_order_number',
                   'order_type', 'order_time', 'passenger', 'payment', 'status', 'bus_start_time',
                   'bus_end_time', 'station_start', 'station_end']
